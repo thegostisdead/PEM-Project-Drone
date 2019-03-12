de92c9624a7bd2c43d33f7b4b0abfa8937ef5061
Caceresenzo <caceresenzo1502@gmail.com>
2019-03-12 10:28:17 +0100

    Added flying path

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -1,5 +1,8 @@
+var mapper;
+
 var position = [ 47.1920857, 2.3766726 ];
 var map;
+
 function initialize() {
 	var latlng = new google.maps.LatLng(position[0], position[1]);
 
@@ -7,30 +10,37 @@ function initialize() {
 	    zoom : 16,
 	    center : latlng,
 	    mapTypeId : google.maps.MapTypeId.HYBRID,
-	    keyboardShortcuts: false
-	});
+	    keyboardShortcuts : false,
+	    scrollwheel : false,
+	    gestureHandling : "none",
+	    panControl : false,
 
-	marker = new google.maps.Marker({
+	    /* The enabled/disabled state of the Fullscreen control. */
+	    fullscreenControl : false,
+
+	    /* The enabled/disabled state of the Zoom control. */
+	    zoomControl : false
+	});
+	
+	mapper = new Mapper(map);
+
+	let marker = new google.maps.Marker({
 	    position : latlng,
 	    map : map,
-	    icon : "./assets/images/drone.png",
-	    title : "Latitude:" + position[0] + " | Longitude:" + position[1]
+	    icon : "./assets/images/drone.png"
 	});
 
 	google.maps.event.addListener(map, 'click', function(event) {
-		map.setCenter(new google.maps.LatLng( event.latLng.lat(), event.latLng.lng()));
+		map.setCenter(event.latLng);
+		marker.setPosition(event.latLng);
+		
+		mapper.appendFlightPlanCoordinates(event.latLng);
 	});
 
 	google.maps.event.addListener(map, 'keypress', function(event) {
-		event =  event || window.event;
-		
-		console.log(event.keyCode);
-	});
+		event = event || window.event;
 
-	google.maps.event.addListener(map, 'mousemove', function(event) {
-		var result = [ event.latLng.lat(), event.latLng.lng() ];
-		transition(result);
-		displayCoordinates(event.latLng);
+		console.log(event.keyCode);
 	});
 }
 
@@ -45,27 +55,31 @@ function displayCoordinates(pnt) {
 	console.log("Latitude: " + lat + "  Longitude: " + lng);
 }
 
-var numDeltas = 100;
-var delay = 10; // milliseconds
-var i = 0;
-var deltaLat;
-var deltaLng;
-
-function transition(result) {
-	i = 0;
-	deltaLat = (result[0] - position[0]) / numDeltas;
-	deltaLng = (result[1] - position[1]) / numDeltas;
-	moveMarker();
-}
-
-function moveMarker() {
-	position[0] += deltaLat;
-	position[1] += deltaLng;
-	var latlng = new google.maps.LatLng(position[0], position[1]);
-	marker.setTitle("Latitude:" + position[0] + " | Longitude:" + position[1]);
-	marker.setPosition(latlng);
-	if (i != numDeltas) {
-		i++;
-		setTimeout(moveMarker, delay);
+class Mapper {
+	
+	constructor(map) {
+		this.map = map;
+		
+		this.initializeFlighPlan();
 	}
+	
+	initializeFlighPlan() {
+		this.flightPath = new google.maps.Polyline({
+	          path: this.flightPlanCoordinates,
+	          geodesic: true,
+	          strokeColor: '#FF0000',
+	          strokeOpacity: 1.0,
+	          strokeWeight: 2
+        });
+
+	    this.flightPath.setMap(this.map);
+	}
+	
+	appendFlightPlanCoordinates(latLng) {
+		this.flightPath.getPath().push(latLng);
+		
+		console.log("Added point to flight plan (" + latLng.lat() + " , " + latLng.lng() + ")");
+	}
+	
+	
 }
