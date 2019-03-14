a6395978d5f8c4d872ff1b7e1df1b8234b6bbe91
Caceresenzo <caceresenzo1502@gmail.com>
2019-03-14 23:51:37 +0100

    Added compatiblity for maven-export
    * Maven suck (low external file support)

--- src/main/java/caceresenzo/server/drone/Application.java
+++ src/main/java/caceresenzo/server/drone/Application.java
@@ -1,16 +1,13 @@
 package caceresenzo.server.drone;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application implements CommandLineRunner {
-	
-	/* Static */
-	private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
 	
 	@Override
 	public void run(String... args) throws Exception {
